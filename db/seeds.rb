14b05dffac155e4db34545c38fc00b9856939a7a
Edisontong <edisonjtong@gmail.com>
2023-01-18 10:27:43 -0700

    added jbuilder and products to db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product = Product.new(name: "race car", price: 1.50, image_url: "image.com", description: "a car that goes really fast")
+product.save
+
+product = Product.new(name: "bouncy ball", price: 0.50, image_url: "pictureofaball.com", description: "a ball that bounces")
+product.save
+
+product = Product.new(name: "dinosaur", price: 2.00, image_url: "dinopics.com", description: "a small plastic dinosaur figure.")
+product.save
