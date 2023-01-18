a9a3da177c84194f3e1af45cc47ac683fb546bdc
Edisontong <edisonjtong@gmail.com>
2023-01-18 13:30:11 -0700

    added tests to test folder and pdated routes and controllers to match naming conventions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
-  def single_product
+  def show
     @product = Product.find_by(id: params["id"])
-    render template: "products/show"
+    render :show
   end
 
-  def all_products
+  def index
     @products = Product.all
-    render template: "products/index"
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/single_product/:id", controller: "products", action: "single_product"
-  get "/all_products", controller: "products", action: "all_products"
+  get "/single_product/:id", controller: "products", action: "show"
+  get "/all_products", controller: "products", action: "index"
 end
