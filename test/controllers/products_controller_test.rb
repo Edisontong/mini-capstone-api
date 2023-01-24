756bc8105eda5259b46aedb3e4e7b827f633d742
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-01-24 12:14:05 -0700

    added happy and sad paths for update and create and tests for both

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,13 +17,17 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "image_url", "description", "is_discounted?", "tax", "total", "created_at", "updated_at"], data.keys
+    assert_equal ["id", "name", "price", "image_url", "description", "is_discounted?", "tax", "total", "top_seller", "inventory_count", "created_at", "updated_at"], data.keys
   end
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test product", price: 1, image_url: "image.jpg", description: "test description" }
+      post "/products.json", params: { name: "test", price: 10, image_url: "test.jpg", description: "test description" }
+      assert_response 200
     end
+
+    post "/products.json", params: {}
+    assert_response 422
   end
 
   test "update" do
@@ -33,9 +37,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     data = JSON.parse(response.body)
     assert_equal "Updated name", data["name"]
-    assert_equal product.price, data["price"]
+    assert_equal product.price.to_s, data["price"]
     assert_equal product.image_url, data["image_url"]
     assert_equal product.description, data["description"]
+
+    patch "/products/#{product.id}.json", params: { name: "" }
+    assert_response 422
   end
 
   test "destroy" do
