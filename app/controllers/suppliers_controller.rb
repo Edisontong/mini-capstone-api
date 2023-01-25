436848cb670491701b3dbd06f849d8c98092ef92
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-01-25 11:38:44 -0700

    added suppliers routes and generated suppliers controller. also created jbuilder for suppliers

---
+++ app/controllers/suppliers_controller.rb
@@ -0,0 +1,46 @@
+class SuppliersController < ApplicationController
+  def show
+    @supplier = Supplier.find_by(id: params["id"])
+    render :show
+  end
+
+  def index
+    @suppliers = Supplier.all
+    render :index
+  end
+
+  def create
+    @supplier = Supplier.create(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+    if @supplier.valid?
+      render :show
+    else
+      render json: { errors: @supplier.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @supplier = Supplier.find_by(id: params["id"])
+    @supplier.update(
+      name: params["name"] || @supplier.name,
+      price: params["price"] || @supplier.price,
+      image_url: params["image_url"] || @supplier.image_url,
+      description: params["description"] || @supplier.description,
+    )
+    if @supplier.valid?
+      render :show
+    else
+      render json: { errors: @supplier.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    supplier = Supplier.find_by(id: params["id"])
+    supplier.destroy
+    render json: { mesage: "The supplier has been obliterated!!!!!!!!!" }
+  end
+end
