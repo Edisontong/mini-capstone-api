a6fd0b59021d21b39b81f6f0f4422de5686380a2
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-01-25 13:30:06 -0700

    started creating model method in products to find images

--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,7 +1,6 @@
 json.id product.id
 json.name product.name
 json.price product.price
-json.image_url product.image_url
 json.description product.description
 json.is_discounted? product.is_discounted?
 json.tax product.tax
@@ -9,5 +8,6 @@ json.total product.total
 json.top_seller product.top_seller
 json.inventory_count product.inventory_count
 json.supplier product.supplier
+json.images product.images
 json.created_at product.created_at
 json.updated_at product.updated_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,35 @@
-Supplier.create!([
-  {name: "test", email: "test@gmail.com", phone_number: "111-222-3333"},
-  {name: "test2", email: "test2@gmail.com", phone_number: "999-555-7777"},
-  {name: "httpie", email: "httpie@server.com", phone_number: "333-444-5555"}
-])
-Product.create!([
-  {name: "race car", price: "1.5", image_url: "image.com", description: "a car that goes really fast", top_seller: true, inventory_count: 100, supplier_id: "1"},
-  {name: "dinosaur", price: "2.0", image_url: "dinopics.com", description: "a small plastic dinosaur figure.", top_seller: true, inventory_count: 50, supplier_id: "1"},
-  {name: "bouncy ball", price: "0.5", image_url: "pictureofaball.com", description: "a ball that bounces", top_seller: true, inventory_count: 300, supplier_id: "1"},
-  {name: "test", price: "20.0", image_url: "test.\ncom", description: "happy birthday to me", top_seller: true, inventory_count: 2000, supplier_id: "2"}
-])
+Supplier.destroy_all
+supplier1 = Supplier.create!(
+  { name: "test", email: "test@gmail.com", phone_number: "111-222-3333" }
+)
+supplier2 = Supplier.create!(
+  { name: "test2", email: "test2@gmail.com", phone_number: "999-555-7777" }
+)
+suppplier3 = Supplier.create!(
+  { name: "httpie", email: "httpie@server.com", phone_number: "333-444-5555" }
+)
+
+Product.destroy_all
+product1 = Product.create!(
+  { name: "race car", price: "1.5", description: "a car that goes really fast", top_seller: true, inventory_count: 100, supplier_id: "1" }
+)
+product2 = Product.create!(
+  { name: "dinosaur", price: "2.0", description: "a small plastic dinosaur figure.", top_seller: true, inventory_count: 50, supplier_id: "1" }
+)
+product3 = Product.create!(
+  { name: "bouncy ball", price: "0.5", description: "a ball that bounces", top_seller: true, inventory_count: 300, supplier_id: "1" }
+)
+product4 = Product.create!(
+  { name: "test", price: "20.0", description: "happy birthday to me", top_seller: true, inventory_count: 2000, supplier_id: "2" },
+)
+
+Image.destroy_all
+Image.create!(
+  url: "image_number_1.com", product_id: product1.id,
+)
+Image.create!(
+  url: "image_number_2.com", product_id: product1.id,
+)
+Image.create!(
+  url: "image_number_3.com", product_id: product1.id,
+)
